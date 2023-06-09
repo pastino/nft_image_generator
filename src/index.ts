aefc5cc4f8255a75ba2b66d85e3b94a21db35991
taejun <joon5006@naver.com>
2023-06-09 20:37:20 +0900

    fix: 이미지 저장 경로 수정

--- src/index.ts
+++ src/index.ts
@@ -71,25 +71,26 @@ const downloadImage = async ({
 
     // 디렉토리가 없으면 생성합니다.
     let baseDirectory = __dirname;
+
     if (IS_PRODUCTION) {
-      // 프로덕션 환경인 경우, /var/www/html/images 폴더를 기본 디렉토리로 사용합니다.
-      baseDirectory = "/var/www/html/images";
+      // 프로덕션 환경인 경우, 상위 디렉토리의 www 폴더를 기본 디렉토리로 사용합니다.
+      baseDirectory = path.join(
+        __dirname,
+        "..",
+        "..",
+        "..",
+        "..",
+        "var",
+        "www",
+        "html",
+        "images",
+        contractAddress
+      );
     }
 
     // if (IS_PRODUCTION) {
-    //   // 프로덕션 환경인 경우, 상위 디렉토리의 www 폴더를 기본 디렉토리로 사용합니다.
-    //   baseDirectory = path.join(
-    //     __dirname,
-    //     "..",
-    //     "..",
-    //     "..",
-    //     "..",
-    //     "var",
-    //     "www",
-    //     "html",
-    //     "images",
-    //     contractAddress
-    //   );
+    //   // 프로덕션 환경인 경우, /var/www/html/images 폴더를 기본 디렉토리로 사용합니다.
+    //   baseDirectory = "/var/www/html/images";
     // }
 
     const originPath = path.join(baseDirectory, "origin");
