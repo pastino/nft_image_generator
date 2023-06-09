7eb05d96c9c825a847305cffe317ece614c7fd1d
taejun <joon5006@naver.com>
2023-06-09 19:56:45 +0900

    fix: 포트 설정 변겨

--- src/index.ts
+++ src/index.ts
@@ -146,6 +146,6 @@ app.post("/image", async (req: Request, res: Response) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on port: "http://localhost:${PORT}"`);
+app.listen(PORT as number, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
