0e3ead39bf60d9a8c2f8022569e5d53a4b0a6f87
taejun <joon5006@naver.com>
2023-11-13 20:08:01 +0900

    fix: 이미지 에러 관련 처리

--- src/index.ts
+++ src/index.ts
@@ -79,6 +79,7 @@ async function processNFTs() {
 
     // 각 NFT에 대해 처리합니다.
     for (const nft of nfts) {
+      console.log("nft", nft);
       // 여기에서 downloadImage 함수를 호출하고 결과에 따라 처리합니다.
       const { isSuccess, message, hashedFileName } = await downloadImage({
         imageUrl:
