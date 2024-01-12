00d7bea996f20dedeff31325131e5db1adb3b6b7
김태준 <gimtaejun@gimtaejun-ui-MacBookPro.local>
2024-01-12 19:45:40 +0900

    fix: current nft id 변경

--- src/index.ts
+++ src/index.ts
@@ -160,7 +160,7 @@ const PORT = IS_PRODUCTION ? process.env.PORT : 9001;
 const app = express();
 app.use(express.json());
 
-let currentNFTId = 1;
+let currentNFTId = 991999;
 const numCPUs = 5;
 
 let connection: amqp.Connection;
