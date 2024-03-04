218b6aa1515a6d98727a65046859f75f007078aa
김태준 <gimtaejun@gimtaejun-ui-MacBookPro.local>
2024-03-04 10:30:28 +0900

    fix: currentNFTId 0부터 다시 시작

--- src/index.ts
+++ src/index.ts
@@ -159,7 +159,7 @@ const PORT = IS_PRODUCTION ? process.env.PORT : 9001;
 const app = express();
 app.use(express.json());
 
-let currentNFTId = 6432573;
+let currentNFTId = 0;
 const numCPUs = 40;
 
 let connection: amqp.Connection;
